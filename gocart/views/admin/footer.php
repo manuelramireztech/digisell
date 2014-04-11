94de6ae72906db334e9af34bd9d92649a340ccc7
Vijay <vijay@phppointofsale.com>
2014-04-11 16:35:25 +0530

    admin design

--- gocart/views/admin/footer.php
+++ gocart/views/admin/footer.php
@@ -1,6 +1,3 @@
-   
-
-
 </div>
 </div>
 <!-- /.content -->
@@ -9,6 +6,10 @@
 </div>
 <!-- /.site-holder -->
  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ 				<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.js');?>"></script>
+            <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui.js');?>"></script>
+            <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+            <script type="text/javascript" src="<?php echo base_url('assets/js/redactor.min.js');?>"></script>
 
                 <!-- Include all compiled plugins (below), or include individual files as needed -->
                 <script src="<?php echo base_url(); ?>js/jquery-ui-1.10.3.custom.min.js"></script>
