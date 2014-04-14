34f7f608e31a4b10c85cb3cdab7ded83884667ed
manoharthedeveloper <manohar@bootstrapguru.com>
2014-04-14 10:19:02 +0530

    orders page fixing

--- gocart/views/admin/orders.php
+++ gocart/views/admin/orders.php
@@ -1,4 +1,6 @@
-<?php
+<div class="row">
+	<div class="col-md-12">
+		<?php
 	//set "code" for searches
 	if(!$code)
 	{
@@ -50,12 +52,10 @@ if ($term):?>
 	}
 </style>
 <div class="row">
-	<div class="span12" style="border-bottom:1px solid #f5f5f5;">
-		<div class="row">
-			<div class="span4">
-				<?php echo $this->pagination->create_links();?>&nbsp;
-			</div>
-			<div class="span8">
+	<div class="col-md-12" style="border-bottom:1px solid #f5f5f5;">
+		
+			
+			
 				<?php echo form_open($this->config->item('admin_folder').'/orders/index', 'class="form-inline" style="float:right"');?>
 					<fieldset>
 						<input id="start_top"  value="" class="span2" type="text" placeholder="Start Date"/>
@@ -65,52 +65,79 @@ if ($term):?>
 				
 						<input id="top" type="text" class="span2" name="term" placeholder="<?php echo lang('term')?>" /> 
 
-						<button class="btn" name="submit" value="search"><?php echo lang('search')?></button>
-						<button class="btn" name="submit" value="export"><?php echo lang('xml_export')?></button>
+						<button class="btn btn-success" name="submit" value="search"><?php echo lang('search')?></button>
+						<button class="btn btn-success" name="submit" value="export"><?php echo lang('xml_export')?></button>
 					</fieldset>
 				</form>
-			</div>
-		</div>
+			
+		
 	</div>
+</div>
+<div class="row">
+	<div class="col-md-4 pull-right">
+				<?php echo $this->pagination->create_links();?>&nbsp;
+			</div>
 </div>
 
 <?php echo form_open($this->config->item('admin_folder').'/orders/bulk_delete', array('id'=>'delete_form', 'onsubmit'=>'return submit_form();', 'class="form-inline"')); ?>
+<div class="panel">
+			<div class="panel-heading">
+				<h3 class="panel-title ">
+					Orders
+					<span class="panel-options">
+						<a href="#" class="panel-minimize">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						<a href="#" class="panel-close">
+							<i class="fa fa-times"></i>
+						</a>
+					</span>
+				</h3>
+			</div>
+			<div class="panel-body">
+				<table class="table table-striped">
+				    <thead>
+						<tr>
+							<th>
+								<input type="checkbox" id="gc_check_all" />
+								<button type="submit" class="btn btn-small btn-danger"><i class="fa fa-trash-o"></i></button>
+							</th>
+							<th><?php echo sort_url('order', 'order_number', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
+							<th><?php echo sort_url('bill_to', 'bill_lastname', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
+							<th><?php echo sort_url('ship_to', 'ship_lastname', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
+							<th><?php echo sort_url('ordered_on','ordered_on', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
+							<th><?php echo sort_url('status','status', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
+							<th><?php echo sort_url('total','total', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
+							
+					    </tr>
+					</thead>
 
-<table class="table table-striped">
-    <thead>
-		<tr>
-			<th><input type="checkbox" id="gc_check_all" /> <button type="submit" class="btn btn-small btn-danger"><i class="icon-trash icon-white"></i></button></th>
-			<th><?php echo sort_url('order', 'order_number', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
-			<th><?php echo sort_url('bill_to', 'bill_lastname', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
-			<th><?php echo sort_url('ship_to', 'ship_lastname', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
-			<th><?php echo sort_url('ordered_on','ordered_on', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
-			<th><?php echo sort_url('status','status', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
-			<th><?php echo sort_url('total','total', $sort_by, $sort_order, $code, $this->config->item('admin_folder')); ?></th>
-			<th></th>
-	    </tr>
-	</thead>
+				    <tbody>
+						<?php echo (count($orders) < 1)?'<tr><td style="text-align:center;" colspan="8">'.lang('no_orders') .'</td></tr>':''?>
+					    <?php foreach($orders as $order): ?>
+						<tr>
+							<td><input name="order[]" type="checkbox" value="<?php echo $order->id; ?>" class="gc_check"/></td>
+							<td><?php echo $order->order_number; ?></td>
+							<td style="white-space:nowrap"><?php echo $order->bill_lastname.', '.$order->bill_firstname; ?></td>
+							<td style="white-space:nowrap"><?php echo $order->ship_lastname.', '.$order->ship_firstname; ?></td>
+							<td style="white-space:nowrap"><?php echo date('m/d/y h:i a', strtotime($order->ordered_on)); ?></td>
+							<td style="span2">
+								<?php echo form_dropdown('status', $this->config->item('order_statuses'), $order->status, 'id="status_form_'.$order->id.'" class="span2" style="float:left;"'); ?>
+								<button type="button" class="btn" onClick="save_status(<?php echo $order->id; ?>)" style="float:left;margin-left:4px;"><?php echo lang('save');?></button>
+							</td>
+							<td><div class="MainTableNotes"><?php echo format_currency($order->total); ?></div></td>
+							<td>
+								<a class="btn btn-small" style="float:right;"href="<?php echo site_url($this->config->item('admin_folder').'/orders/order/'.$order->id);?>"><i class="icon-search"></i> <?php echo lang('form_view')?></a>
+							</td>
+						</tr>
+					    <?php endforeach; ?>
+    						</tbody>
+				</table>
+				
+			</div>
+			<!-- /panel body -->
+		</div>
 
-    <tbody>
-	<?php echo (count($orders) < 1)?'<tr><td style="text-align:center;" colspan="8">'.lang('no_orders') .'</td></tr>':''?>
-    <?php foreach($orders as $order): ?>
-	<tr>
-		<td><input name="order[]" type="checkbox" value="<?php echo $order->id; ?>" class="gc_check"/></td>
-		<td><?php echo $order->order_number; ?></td>
-		<td style="white-space:nowrap"><?php echo $order->bill_lastname.', '.$order->bill_firstname; ?></td>
-		<td style="white-space:nowrap"><?php echo $order->ship_lastname.', '.$order->ship_firstname; ?></td>
-		<td style="white-space:nowrap"><?php echo date('m/d/y h:i a', strtotime($order->ordered_on)); ?></td>
-		<td style="span2">
-			<?php echo form_dropdown('status', $this->config->item('order_statuses'), $order->status, 'id="status_form_'.$order->id.'" class="span2" style="float:left;"'); ?>
-			<button type="button" class="btn" onClick="save_status(<?php echo $order->id; ?>)" style="float:left;margin-left:4px;"><?php echo lang('save');?></button>
-		</td>
-		<td><div class="MainTableNotes"><?php echo format_currency($order->total); ?></div></td>
-		<td>
-			<a class="btn btn-small" style="float:right;"href="<?php echo site_url($this->config->item('admin_folder').'/orders/order/'.$order->id);?>"><i class="icon-search"></i> <?php echo lang('form_view')?></a>
-		</td>
-	</tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
 
 </form>
 <script type="text/javascript">
@@ -186,4 +213,6 @@ function hide_animation()
 	<div id="saving" style="background-color:#000; position:fixed; width:100%; height:100%; top:0px; left:0px;z-index:100000"></div>
 	<img id="saving_animation" src="<?php echo base_url('assets/img/storing_animation.gif');?>" alt="saving" style="z-index:100001; margin-left:-32px; margin-top:-32px; position:fixed; left:50%; top:50%"/>
 	<div id="saving_text" style="text-align:center; width:100%; position:fixed; left:0px; top:50%; margin-top:40px; color:#fff; z-index:100001"><?php echo lang('saving');?></div>
+</div>
+	</div>
 </div>
