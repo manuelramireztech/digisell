c41ccfb329e47aa0deb40263357ca642f8dfe1f8
Vijay <vijay@phppointofsale.com>
2014-04-17 11:44:38 +0530

    save categoroies

--- gocart/views/admin/category_form.php
+++ gocart/views/admin/category_form.php
@@ -1,110 +1,159 @@
-<?php echo form_open_multipart($this->config->item('admin_folder').'/categories/form/'.$id); ?>
+<div class="row">
+	<div class="col-md-12">
+		<?php echo form_open_multipart($this->config->item('admin_folder').'/categories/form/'.$id, 'class="form-horizontal"'); ?>
+		<div class="panel">
+			<div class="panel-heading">
+				<h3 class="panel-title ">
+					<?php echo lang('category_form') ?>
+					<span class="panel-options">
+						<a href="#" class="panel-minimize">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						<a href="#" class="panel-close">
+							<i class="fa fa-times"></i>
+						</a>
+					</span>
+				</h3>
+			</div>
+			<div class="panel-body">
+				<div class="tabbable">
 
-<div class="tabbable">
+					<ul class="nav nav-tabs">
+						<li class="active"><a href="#description_tab" data-toggle="tab"><?php echo lang('description');?></a></li>
+						<li><a href="#attributes_tab" data-toggle="tab"><?php echo lang('attributes');?></a></li>
+						<li><a href="#seo_tab" data-toggle="tab"><?php echo lang('seo');?></a></li>
+					</ul>
 
-	<ul class="nav nav-tabs">
-		<li class="active"><a href="#description_tab" data-toggle="tab"><?php echo lang('description');?></a></li>
-		<li><a href="#attributes_tab" data-toggle="tab"><?php echo lang('attributes');?></a></li>
-		<li><a href="#seo_tab" data-toggle="tab"><?php echo lang('seo');?></a></li>
-	</ul>
+					<div class="tab-content navbar-category">
+						<div class="tab-pane active" id="description_tab">
+							<div class="form-group ">
+								<label for="name" class="col-sm-2 control-label"><?php echo lang('name');?></label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array('name'=>'name', 'value'=>set_value('name', $name), 'class'=>'form-control');
+									echo form_input($data);
+									?>
+								</div>
+							</div>
+							<div class="form-group ">
+								<label for="description" class="col-sm-2 control-label"><?php echo lang('description');?></label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array('name'=>'description', 'class'=>'redactor form-control', 'style'=>'margin-left:10px', 'value'=>set_value('description', $description));
+									echo form_textarea($data);
+									?>
+								</div>
+							</div>
+							<div class="form-group ">
+								<label for="enabled" class="col-sm-2 control-label"><?php echo lang('enabled');?> </label>
+								<div class="col-sm-10">
+									<?php echo form_dropdown('enabled', array('0' => lang('disabled'), '1' => lang('enabled')),  set_value('enabled',$enabled)); ?>
+								</div>
+							</div>
+						</div>
 
-	<div class="tab-content">
-		<div class="tab-pane active" id="description_tab">
-			
-			<fieldset>
-				<label for="name"><?php echo lang('name');?></label>
-				<?php
-				$data	= array('name'=>'name', 'value'=>set_value('name', $name), 'class'=>'span12');
-				echo form_input($data);
-				?>
-				
-				<label for="description"><?php echo lang('description');?></label>
-				<?php
-				$data	= array('name'=>'description', 'class'=>'redactor', 'value'=>set_value('description', $description));
-				echo form_textarea($data);
-				?>
+						<div class="tab-pane" id="attributes_tab">
+							<div class="form-group">
+								<label for="slug" class="col-sm-2 control-label"><?php echo lang('slug');?> </label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array('name'=>'slug', 'value'=>set_value('slug', $slug), 'class'=>'form-control');
+									echo form_input($data);
+									?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="sequence" class="col-sm-2 control-label"><?php echo lang('sequence');?> </label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array('name'=>'sequence', 'value'=>set_value('sequence', $sequence), 'class'=>'form-control');
+									echo form_input($data);
+									?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="parent_id" class="col-sm-2 control-label"><?php echo lang('parent');?> </label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array(0 => lang('top_level_category'));
+									foreach($categories as $parent)
+									{
+										if($parent->id != $id)
+										{
+											$data[$parent->id] = $parent->name;
+										}
+									}
+									echo form_dropdown('parent_id', $data, $parent_id);
+									?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="excerpt" class="col-sm-2 control-label"><?php echo lang('excerpt');?> </label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array('name'=>'excerpt', 'value'=>set_value('excerpt', $excerpt), 'class'=>'form-control', 'rows'=>3);
+									echo form_textarea($data);
+									?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="image" class="col-sm-2 control-label"><?php echo lang('image');?> </label>
+								<div class="col-sm-10">
+									<div class="input-append">
+										<?php echo form_upload(array('name'=>'image'));?><span class="add-on"><?php echo lang('max_file_size');?> <?php echo  $this->config->item('size_limit')/1024; ?>kb</span>
+									</div>
 
-				<label for="enabled"><?php echo lang('enabled');?> </label>
-        		<?php echo form_dropdown('enabled', array('0' => lang('disabled'), '1' => lang('enabled')), set_value('enabled',$enabled)); ?>
-			</fieldset>
-		</div>
+									<?php if($id && $image != ''):?>
 
-		<div class="tab-pane" id="attributes_tab">
-			
-			<fieldset>
-				<label for="slug"><?php echo lang('slug');?> </label>
-				<?php
-				$data	= array('name'=>'slug', 'value'=>set_value('slug', $slug));
-				echo form_input($data);
-				?>
-				
-				<label for="sequence"><?php echo lang('sequence');?> </label>
-				<?php
-				$data	= array('name'=>'sequence', 'value'=>set_value('sequence', $sequence));
-				echo form_input($data);
-				?>
-				
-				<label for="parent_id"><?php echo lang('parent');?> </label>
-				<?php
-				$data	= array(0 => lang('top_level_category'));
-				foreach($categories as $parent)
-				{
-					if($parent->id != $id)
-					{
-						$data[$parent->id] = $parent->name;
-					}
-				}
-				echo form_dropdown('parent_id', $data, $parent_id);
-				?>
-				
-				<label for="excerpt"><?php echo lang('excerpt');?> </label>
-				<?php
-				$data	= array('name'=>'excerpt', 'value'=>set_value('excerpt', $excerpt), 'class'=>'span12', 'rows'=>3);
-				echo form_textarea($data);
-				?>
+										<div style="text-align:center; padding:5px; border:1px solid #ddd;"><img src="<?php echo base_url('uploads/images/small/'.$image);?>" alt="current"/><br/><?php echo lang('current_file');?></div>
 
-				<label for="image"><?php echo lang('image');?> </label>
-				<div class="input-append">
-					<?php echo form_upload(array('name'=>'image'));?><span class="add-on"><?php echo lang('max_file_size');?> <?php echo  $this->config->item('size_limit')/1024; ?>kb</span>
+									<?php endif;?>
+								</div>
+							</div>
+						</div>
+
+						<div class="tab-pane" id="seo_tab">
+							<div class="form-group">
+								<label for="seo_title" class="col-sm-2 control-label"><?php echo lang('seo_title');?> </label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array('name'=>'seo_title', 'value'=>set_value('seo_title', $seo_title), 'class'=>'form-control');
+									echo form_input($data);
+									?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-2 control-label"><?php echo lang('meta');?></label>
+								<div class="col-sm-10">
+									<?php
+									$data	= array('rows'=>3, 'name'=>'meta', 'value'=>set_value('meta', html_entity_decode($meta)), 'class'=>'form-control');
+									echo form_textarea($data);
+									?>
+									<label class="label label-info"><?php echo lang('meta_data_description');?></label>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
-					
-				<?php if($id && $image != ''):?>
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<button type="submit" class="btn btn-primary"><?php echo lang('save');?></button>
+					</div>
+				</div>
 				
-				<div style="text-align:center; padding:5px; border:1px solid #ddd;"><img src="<?php echo base_url('uploads/images/small/'.$image);?>" alt="current"/><br/><?php echo lang('current_file');?></div>
 				
-				<?php endif;?>
-				
-			</fieldset>
-			
+			</div>
+
+			<!-- /panel body -->
 		</div>
-		
-		<div class="tab-pane" id="seo_tab">
-			<fieldset>
-				<label for="seo_title"><?php echo lang('seo_title');?> </label>
-				<?php
-				$data	= array('name'=>'seo_title', 'value'=>set_value('seo_title', $seo_title), 'class'=>'span12');
-				echo form_input($data);
-				?>
-				
-				<label><?php echo lang('meta');?></label> 
-				<?php
-				$data	= array('rows'=>3, 'name'=>'meta', 'value'=>set_value('meta', html_entity_decode($meta)), 'class'=>'span12');
-				echo form_textarea($data);
-				?>
-				<p class="help-block"><?php echo lang('meta_data_description');?></p>
-			</fieldset>
-		</div>
-	</div>
 
+	</form>
+
+	<script type="text/javascript">
+		$('form').submit(function() {
+			$('.btn').attr('disabled', true).addClass('disabled');
+		});
+	</script>
 </div>
-
-<div class="form-actions">
-	<button type="submit" class="btn btn-primary"><?php echo lang('save');?></button>
 </div>
-</form>
-
-<script type="text/javascript">
-$('form').submit(function() {
-	$('.btn').attr('disabled', true).addClass('disabled');
-});
-</script>
--- gocart/views/admin/organize_category.php
+++ gocart/views/admin/organize_category.php
@@ -1,3 +1,56 @@
+<div class="row">
+	<div class="col-md-12">
+		
+
+		<div class="alert alert-info">
+			<?php echo lang('drag_and_drop');?>
+		</div>
+		<div class="panel">
+			<div class="panel-heading">
+				<h3 class="panel-title ">
+					<?php echo lang('organize') ?>
+					<span class="panel-options">
+						<a href="#" class="panel-minimize">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						<a href="#" class="panel-close">
+							<i class="fa fa-times"></i>
+						</a>
+					</span>
+				</h3>
+			</div>
+			<div class="panel-body">
+				<div class="table-responsive">
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th><?php echo lang('sku');?></th>
+								<th><?php echo lang('name');?></th>
+								<th><?php echo lang('price');?></th>
+								<th><?php echo lang('sale');?></th>
+							</tr>
+						</thead>
+						<tbody id="category_contents">
+							<?php foreach ($category_products as $product):?>
+								<tr id="product-<?php echo $product->id;?>">
+									<td><?php echo $product->sku;?></td>
+									<td><?php echo $product->name;?></td>
+									<td><?php echo format_currency($product->price);?></td>
+									<td><?php echo format_currency($product->saleprice);?></td>
+								</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+				</div>
+				
+				
+			</div>
+
+			<!-- /panel body -->
+		</div>
+		
+	</div>
+</div>
 <script type="text/javascript">
 //<![CDATA[
 $(document).ready(function(){
@@ -28,7 +81,7 @@ function create_sortable()
 function save_sortable()
 {
 	serial=$('#category_contents').sortable('serialize');
-			
+
 	$.ajax({
 		url:'<?php echo site_url($this->config->item('admin_folder').'/categories/process_organization/'.$category->id);?>',
 		type:'POST',
@@ -37,28 +90,3 @@ function save_sortable()
 }
 //]]>
 </script>
-
-<div class="alert alert-info">
-	<?php echo lang('drag_and_drop');?>
-</div>
-
-<table class="table table-striped">
-	<thead>
-		<tr>
-			<th><?php echo lang('sku');?></th>
-			<th><?php echo lang('name');?></th>
-			<th><?php echo lang('price');?></th>
-			<th><?php echo lang('sale');?></th>
-		</tr>
-	</thead>
-	<tbody id="category_contents">
-<?php foreach ($category_products as $product):?>
-		<tr id="product-<?php echo $product->id;?>">
-			<td><?php echo $product->sku;?></td>
-			<td><?php echo $product->name;?></td>
-			<td><?php echo format_currency($product->price);?></td>
-			<td><?php echo format_currency($product->saleprice);?></td>
-		</tr>
-<?php endforeach; ?>
-	</tbody>
-</table>
