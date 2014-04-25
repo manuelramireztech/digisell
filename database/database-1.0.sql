9455f94ab00770906d5ab79a32f3e21a9894baac
vijay <vijay@phppointofsale.com>
2014-04-25 15:39:26 +0530

    save

--- database/database-1.0.sql
+++ database/database-1.0.sql
@@ -1,3 +1,4 @@
 ALTER TABLE  `customers` CHANGE  `default_billing_address`  `default_billing_address` INT( 9 ) NULL DEFAULT  '0';
 ALTER TABLE  `customers` CHANGE  `default_shipping_address`  `default_shipping_address` INT( 9 ) NULL DEFAULT  '0';
 ALTER TABLE  `customers_address_bank` CHANGE  `entry_name`  `entry_name` VARCHAR( 20 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
+ALTER TABLE  `routes` CHANGE  `route`  `route` VARCHAR( 32 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
