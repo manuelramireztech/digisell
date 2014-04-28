b83c1fe6e5386bde23609c1cc43866770145d118
vijay <vijay@phppointofsale.com>
2014-04-28 16:09:43 +0530

    save

--- database/database-1.0.sql
+++ database/database-1.0.sql
@@ -6,3 +6,4 @@ ALTER TABLE  `routes` CHANGE  `route`  `route` VARCHAR( 32 ) CHARACTER SET utf8 
 ALTER TABLE `orders` CHANGE `order_number` `order_number` VARCHAR(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `orders` CHANGE `shipped_on` `shipped_on` DATETIME NULL DEFAULT NULL;
 ALTER TABLE `download_package_files` CHANGE `downloads` `downloads` INT(5) NOT NULL DEFAULT '0';
+ALTER TABLE  `pages` CHANGE  `url`  `url` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
