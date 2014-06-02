723bec15dc8b306bd0200b786a9c9430d03faa64
vijay <vijay@phppointofsale.com>
2014-06-03 04:58:01 +0530

    save

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'password123';
 $db['default']['database'] = 'digiaudit';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'phpaudit_';
